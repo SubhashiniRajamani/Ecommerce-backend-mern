bd2dc284f49725403c07ff186cc31af8980c38b6
Subhashini Rajamani <subhy1998@gmail.com>
2026-01-22 12:06:24 +0530

    Update before_install.sh to change directory
    Change directory before deleting old files.

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,4 +3,5 @@ echo "Stopping old app"
 pm2 stop all || true
 
 echo "Deleting old node_modules"
-rm -rf /home/ubuntu/Ecommerce-backend-mern-main/node_modules
+cd /home/ubuntu/Ecommerce-backend-mern
+rm -rf *
