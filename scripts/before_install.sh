e2237e762814a4244be6885fd4e51e10b2a5f18a
Subhashini Rajamani <subhy1998@gmail.com>
2026-01-28 17:55:57 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Stopping old app"
-pm2 stop all
+pm2 delete all
 
 echo "Deleting old node_modules"
 cd /home/ubuntu/Ecommerce-backend-mern-main
-rm -rf *
+rm -rf node_modules/ package-lock.json
