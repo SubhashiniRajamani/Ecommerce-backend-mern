b6545c9d95291ee1b25d0ca6a6ef9dd7e2140236
Subhashini Rajamani <subhy1998@gmail.com>
2026-01-30 16:00:11 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-cd /home/ubuntu/Ecommerce-backend-mern-main
+set -e
 
-pm2 start 'node app.js'
+AWS_REGION=us-east-1
+AWS_ACCOUNT_ID=YOUR_ACCOUNT_ID
+IMAGE_NAME=ecommerce-backend
+
+echo "Logging in to ECR..."
+aws ecr get-login-password --region $AWS_REGION | \
+docker login --username AWS --password-stdin \
+$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+echo "Pulling latest image..."
+docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME:latest
+
+echo "Running backend container..."
+docker run -d \
+  --name ecommerce-backend \
+  -p 5000:5000 \
+  --restart always \
+  $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME:latest
