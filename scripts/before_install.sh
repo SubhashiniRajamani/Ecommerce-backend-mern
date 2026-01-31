8ee1637019ae0aa5c139ae0fa5e2e7559a043ef7
Subhashini Rajamani <subhy1998@gmail.com>
2026-01-31 15:52:27 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,7 +3,8 @@ set -e
 
 echo "Stopping existing backend container (if any)..."
 
-docker rm -f 313530061217.dkr.ecr.us-east-1.amazonaws.com/ecommerce-backend
+docker rm -f 313530061217.dkr.ecr.us-east-1.amazonaws.com/ecommerce-backend || true
+docker rm -f ecommerce-backend
 
 echo "Cleaning unused Docker images..."
 docker system prune -af || true
